4177a0bc8b9edb73a20145fdd37a208e2cadeb08
Brian Stasiukaitis <brianstas@gmail.com>
2024-08-28 08:21:04 -0400

    added axios and TMDB call
    Added axios to make the TMDB calls easier since they're based on URLs. Also added he preliminary call to TMDB in order to get relevant movies according to the query input by the user.

--- src/app/api/api.tsx
+++ src/app/api/api.tsx
@@ -1,7 +1,11 @@
+import axios from 'axios';
+
 import * as streamingAvailability from "streaming-availability";
 require('dotenv').config()
 
 const RAPID_API_KEY = process.env.RAPID_API_KEY
+const tmdbApiKey = process.env.TMDB_API_KEY
+const tmdbBaseUrl = 'https://api.themoviedb.org/3'
 
 export const client = new streamingAvailability.Client(new streamingAvailability.Configuration({
 	apiKey: RAPID_API_KEY
@@ -16,3 +20,28 @@ export const client = new streamingAvailability.Client(new streamingAvailability
 // show.streamingOptions["us"].forEach((streamingOption) => {
 // 	console.log(streamingOption.link);
 // });
+
+export async function fetchMovies(query) {
+	try {
+	  const response = await axios.get(`${tmdbBaseUrl}/search/movie`, {
+		params: {
+		  api_key: tmdbApiKey,
+		  query: query,
+		  language: 'en-US',
+		  page: 1,
+		  include_adult: false,
+		},
+	  });
+  
+	  // Extract relevant data
+	  const movies = response.data.results.map(movie => ({
+		id: movie.id,
+		title: movie.title,
+	  }));
+  
+	  return movies;
+	} catch (error) {
+	  console.error('Error fetching movies:', error);
+	  return [];
+	}
+  }
