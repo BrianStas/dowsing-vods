3756512b40352a2b88e1be13b8f1958b762c786b
Brian Stasiukaitis <brianstas@gmail.com>
2024-08-29 03:25:15 -0400

    placeholder search
    Added search component to add in front page in order to pull input to search TMDB. Will need extensive testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { client } from "./api/api";
 export default async function Home() {
 
   let show = await client.showsApi.getShow(
-	{id: "tt0068646", country: "us"}
+	{id: "tt3393786", country: "us"}
 );
 
 console.log(show.title);
 console.log(show.overview);
-const streamChoice: string = show.streamingOptions["us"][1].link;
+const streamChoice: string = show.streamingOptions["us"][0].link;
 //tmdb will provide the ID used to get the streaming options from streamingAvailability
 // Need to find quickest route to obtain list of movies with IDs from TMDB
 
