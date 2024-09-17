289582449bdc555d024bf18493b1fa52e16b93e4
Brian Stasiukaitis <brianstas@gmail.com>
2024-09-16 23:57:32 -0400

    consoles fixed
    swapped console logs to get further information. Use Postman for further listing of properties from TMDB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home({
 	{id: "movie/343611", country: "us"}
 );
 
-console.log(show.title);
+console.log(show);
 console.log(show.overview);
 const streamChoice: string = show.streamingOptions["us"][0].link;
 
@@ -23,6 +23,7 @@ const streamChoice: string = show.streamingOptions["us"][0].link;
 
 // to search availability by TMDB ID it must be "movie/{id}"
 // search url would be fetch('https://api.themoviedb.org/3/search/movie?query={searchTerm}&include_adult=false&language=en-US&page=1', options)
+// check sources
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
